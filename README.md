e8ded3a74294b1c082188432dd786c97b09ddb30
Oleksii Velychko <oleksiivelychko@icloud.com>
2021-11-12 07:31:23 +0200

    insertion sort package

--- README.md
+++ README.md
@@ -25,3 +25,5 @@ Remove  |__O(n)__|__O(1)__|
 📌 <a href="factorial/factorial.go">Factorial</a>
 
 📌 <a href="fibonacci/fibonacci.go">Fibonacci</a>
+
+📌 <a href="insertion_sort/insertion_sort.go">Insertion sort</a>
---
+++ insertion_sort/insertion_sort.go
@@ -0,0 +1,40 @@
+package selection_sort
+
+import (
+	"github.com/oleksiivelychko/go-helper/pop"
+)
+
+/*InsertionSort
+
+O(n^2)
+
+1. Find the smallest element in array.
+2. Put the smallest element into new array.
+3. Remove the smallest element from array.
+*/
+func InsertionSort(slice []int) []int {
+	var newSlice = make([]int, 3)
+
+	var length = len(slice)
+	for i := 0; i < length; i++ {
+		smallestIndex := findSmallestIndex(slice)
+		newSlice[i] = slice[smallestIndex]
+		slice = pop.IntPop(slice, smallestIndex)
+	}
+
+	return newSlice
+}
+
+func findSmallestIndex(slice []int) int {
+	var smallestIndex = 0
+	var smallest = slice[0]
+
+	for i := 1; i <= len(slice)-1; i++ {
+		if slice[i] < smallest {
+			smallestIndex = i
+			smallest = slice[i]
+		}
+	}
+
+	return smallestIndex
+}
