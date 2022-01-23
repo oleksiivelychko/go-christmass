db21dc0564c69447c6dc5b9dd406461f192c0b4e
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-01-23 08:04:09 +0200

    remove description

--- two_sum/two_sum.go
+++ two_sum/two_sum.go
@@ -3,16 +3,6 @@ package two_sum
 /*
 TwoSum
 https://leetcode.com/problems/two-sum/
-
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-You can return the answer in any order.
-
-Constraints:
-2 <= nums.length <= 104
--10^9 <= nums[i] <= 109
--10^9 <= target <= 109
-Only one valid answer exists.
 */
 func TwoSum(nums []int, target int) []int {
 	sums := make([]int, 0, 2)
