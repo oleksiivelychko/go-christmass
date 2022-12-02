1e632fbc42cf1d21e80c99f4be1c7046bbb653db
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-12-02 10:03:18 +0200

    cover subset sum by benchmark

--- subset_sum/subset_sum_test.go
+++ subset_sum/subset_sum_test.go
@@ -1,11 +1,23 @@
 package subset_sum
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
 
 func TestSubsetSum(t *testing.T) {
 	set := []int{3, 4, 5, 2}
 	isSubsetSum := SubsetSum(set, len(set)-1, 9)
 	if isSubsetSum != true {
 		t.Errorf("[func SubsetSum(set []int, n, sum int) bool] -> %t != true", isSubsetSum)
+	}
+}
+
+func BenchmarkSubsetSum(b *testing.B) {
+	rand.Seed(time.Now().Unix())
+	for i := 0; i < b.N; i++ {
+		set := rand.Perm(5)
+		SubsetSum(set, len(set)-1, rand.Int())
 	}
 }
