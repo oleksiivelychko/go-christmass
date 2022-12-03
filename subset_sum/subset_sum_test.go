01fccceacf5b7f058de09fdcd0e3e0f9f887a97e
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-12-03 19:17:27 +0200

    add cases to subset sum tests

--- subset_sum/subset_sum_test.go
+++ subset_sum/subset_sum_test.go
@@ -8,9 +8,30 @@ import (
 
 func TestSubsetSum(t *testing.T) {
 	set := []int{3, 4, 5, 2}
+
 	isSubsetSum := SubsetSum(set, len(set)-1, 9)
 	if isSubsetSum != true {
 		t.Errorf("[func SubsetSum(set []int, n, sum int) bool] -> %t != true", isSubsetSum)
+	}
+
+	isSubsetSum = SubsetSum(set, len(set)-1, 10)
+	if isSubsetSum != true {
+		t.Errorf("[func SubsetSum(set []int, n, sum int) bool] -> %t != true", isSubsetSum)
+	}
+
+	isSubsetSum = SubsetSum(set, len(set)-1, 11)
+	if isSubsetSum != true {
+		t.Errorf("[func SubsetSum(set []int, n, sum int) bool] -> %t != true", isSubsetSum)
+	}
+
+	isSubsetSum = SubsetSum(set, len(set)-1, 12)
+	if isSubsetSum != true {
+		t.Errorf("[func SubsetSum(set []int, n, sum int) bool] -> %t != true", isSubsetSum)
+	}
+
+	isSubsetSum = SubsetSum(set, len(set)-1, 13)
+	if isSubsetSum != false {
+		t.Errorf("[func SubsetSum(set []int, n, sum int) bool] -> %t != false", isSubsetSum)
 	}
 }
 
