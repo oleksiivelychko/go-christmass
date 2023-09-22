03f16bab7ed4de2ea631f1ddf39dc40ed95d3605
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-09-22 13:38:43 +0300

    add comments

--- binary_search/binary_search.go
+++ binary_search/binary_search.go
@@ -15,26 +15,24 @@ type INumber interface {
 /*
 BinarySearch O(log n)
 log2(8)=3 attempts are required to find value (operation opposite to exponentiation).
-
 Dataset must be sorted and contains numbers only.
-
-1. pick the middle item and compare it to search value
-2. if picked value less than search one then keep searching on the right side of dataset
-3. if picked value greater than search one then keep searching on the left side of dataset
-4. repeat steps until search value will not be found or dataset is not empty
 */
-func BinarySearch[T INumber](slice []T, target T) (bool, int) {
+func BinarySearch[T INumber](dataset []T, searchValue T) (bool, int) {
 	var bottom = 0
-	var top = len(slice) - 1
+	var top = len(dataset) - 1
 
 	for bottom < top {
+		// pick the middle item
 		middle := (top + bottom) / 2
 
-		if slice[middle] == target {
+		// compare it to search value
+		if dataset[middle] == searchValue {
 			return true, middle
-		} else if slice[middle] > target {
+		} else if dataset[middle] > searchValue {
+			// keep searching on the left side of dataset
 			top = middle - 1
-		} else if slice[middle] < target {
+		} else if dataset[middle] < searchValue {
+			// keep searching on the right side of dataset
 			bottom = middle + 1
 		}
 	}
@@ -42,24 +40,24 @@ func BinarySearch[T INumber](slice []T, target T) (bool, int) {
 	return false, 0
 }
 
-func BinarySearchRecursion[T INumber](slice []T, target T) bool {
+func BinarySearchRecursion[T INumber](dataset []T, searchValue T) bool {
 	var bottom = 0
-	var top = len(slice) - 1
+	var top = len(dataset) - 1
 	var middle = -1
 	var found = false
 
 	if bottom < top {
 		middle = (bottom + top) / 2
 
-		if slice[middle] == target {
+		if dataset[middle] == searchValue {
 			return true
-		} else if slice[middle] > target {
+		} else if dataset[middle] > searchValue {
 			top = middle - 1
-		} else if slice[middle] < target {
+		} else if dataset[middle] < searchValue {
 			bottom = middle + 1
 		}
 
-		found = BinarySearchRecursion(slice[bottom:top+1], target)
+		found = BinarySearchRecursion(dataset[bottom:top+1], searchValue)
 	}
 
 	return found
