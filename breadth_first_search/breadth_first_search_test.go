5d72b68888104a923569fec4337f0cf1937923fd
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-09-23 13:21:11 +0300

    edit fixtures

--- breadth_first_search/breadth_first_search_test.go
+++ breadth_first_search/breadth_first_search_test.go
@@ -3,24 +3,23 @@ package breadth_first_search
 import "testing"
 
 /*
-V(you) 				-> V(rabbit)
-V(you) 				-> V(piglet)
-V(you) 				-> V(winnie-the-pooh)
-V(piglet)			-> V(gopher)
-V(piglet) 			-> V(eeyore)
-V(rabbit) 			-> V(tiger)
+V(robin) 			-> V(rabbit)
+V(robin) 			-> V(piglet)
+V(robin) 			-> V(winnie-the-pooh)
+V(piglet)			-> V(eeyore)
 V(winnie-the-pooh)	-> V(kanga)
 V(winnie-the-pooh)	-> V(roo)
+V(rabbit) 			-> V(tiger)
+V(rabbit) 			-> V(winnie-the-pooh)
 V(tigger)			-> V(owl)
-V(tigger)			-> V(gopher)
 */
 var unweightedGraph = map[string][]string{
-	"you":             {"rabbit", "piglet", "winnie-the-pooh"},
-	"piglet":          {"gopher", "eeyore"},
-	"rabbit":          {"tigger"},
+	"robin":           {"rabbit", "piglet", "winnie-the-pooh"},
+	"piglet":          {"eeyore"},
+	"rabbit":          {"tigger", "winnie-the-pooh"},
 	"winnie-the-pooh": {"kanga", "roo"},
 	"eeyore":          {},
-	"tigger":          {"owl", "gopher"},
+	"tigger":          {"owl"},
 	"kanga":           {},
 	"roo":             {},
 	"owl":             {},
@@ -28,7 +27,7 @@ var unweightedGraph = map[string][]string{
 }
 
 func TestBreadthFirstSearch(t *testing.T) {
-	node := BreadthFirstSearch(unweightedGraph, "gopher", "you")
+	node := BreadthFirstSearch(unweightedGraph, "owl", "robin")
 	if node == "" {
 		t.Error("unable to find node")
 	}
@@ -36,6 +35,6 @@ func TestBreadthFirstSearch(t *testing.T) {
 
 func BenchmarkBreadthFirstSearch(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		BreadthFirstSearch(unweightedGraph, "gopher", "you")
+		BreadthFirstSearch(unweightedGraph, "owl", "robin")
 	}
 }
