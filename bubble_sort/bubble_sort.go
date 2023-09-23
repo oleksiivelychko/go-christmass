9860202584a2f724ff00eb53694463e1269a3c3b
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-09-23 19:12:06 +0300

    edit var name

--- bubble_sort/bubble_sort.go
+++ bubble_sort/bubble_sort.go
@@ -3,13 +3,13 @@ package sort
 /*
 BubbleSort O(n^2)
 */
-func BubbleSort(slice []int) []int {
-	for i := 0; i < len(slice); i++ {
-		for j := 0; j < len(slice)-i-1; j++ {
-			if slice[j] > slice[j+1] {
-				slice[j], slice[j+1] = slice[j+1], slice[j]
+func BubbleSort(dataset []int) []int {
+	for i := 0; i < len(dataset); i++ {
+		for j := 0; j < len(dataset)-i-1; j++ {
+			if dataset[j] > dataset[j+1] {
+				dataset[j], dataset[j+1] = dataset[j+1], dataset[j]
 			}
 		}
 	}
-	return slice
+	return dataset
 }
