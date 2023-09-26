f5b173e2805897f2e85f78a0694cb1f954dd2dba
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-09-26 08:43:44 +0300

    edit error message

--- bubble_sort/bubble_sort_test.go
+++ bubble_sort/bubble_sort_test.go
@@ -10,7 +10,7 @@ var unsorted = [10]int{11, -13, 15, 17, 19, 1, 3, 5, -7, 9}
 func TestBubbleSort(t *testing.T) {
 	sorted := BubbleSort(unsorted[:])
 	if !reflect.DeepEqual([]int{-13, -7, 1, 3, 5, 9, 11, 15, 17, 19}, sorted) {
-		t.Error("arrays are not equal")
+		t.Error("dataset was not sorted")
 	}
 }
 
