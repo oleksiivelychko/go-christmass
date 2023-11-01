dac6ffd4eb7371b842af2b5ee873061f92faab08
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-11-01 10:05:33 +0200

    naming convention

--- fibonacci/fibonacci_test.go
+++ fibonacci/fibonacci_test.go
@@ -6,37 +6,37 @@ func TestFibonacci(t *testing.T) {
 	f := fibonacci()
 
 	for i := 0; i < 10; i++ {
-		sequenceNum := f()
+		num := f()
 
-		if i == 0 && sequenceNum != 0 {
-			t.Errorf("[func Fibonacci() func() int] -> %d != 0", sequenceNum)
+		if i == 0 && num != 0 {
+			t.Errorf("[func Fibonacci() func() int] -> %d != 0", num)
 		}
-		if i == 1 && sequenceNum != 1 {
-			t.Errorf("[func Fibonacci() func() int] -> %d != 1", sequenceNum)
+		if i == 1 && num != 1 {
+			t.Errorf("[func Fibonacci() func() int] -> %d != 1", num)
 		}
-		if i == 2 && sequenceNum != 1 {
-			t.Errorf("[func Fibonacci() func() int] -> %d != 1", sequenceNum)
+		if i == 2 && num != 1 {
+			t.Errorf("[func Fibonacci() func() int] -> %d != 1", num)
 		}
-		if i == 3 && sequenceNum != 2 {
-			t.Errorf("[func Fibonacci() func() int] -> %d != 2", sequenceNum)
+		if i == 3 && num != 2 {
+			t.Errorf("[func Fibonacci() func() int] -> %d != 2", num)
 		}
-		if i == 4 && sequenceNum != 3 {
-			t.Errorf("[func Fibonacci() func() int] -> %d != 3", sequenceNum)
+		if i == 4 && num != 3 {
+			t.Errorf("[func Fibonacci() func() int] -> %d != 3", num)
 		}
-		if i == 5 && sequenceNum != 5 {
-			t.Errorf("[func Fibonacci() func() int] -> %d != 5", sequenceNum)
+		if i == 5 && num != 5 {
+			t.Errorf("[func Fibonacci() func() int] -> %d != 5", num)
 		}
-		if i == 6 && sequenceNum != 8 {
-			t.Errorf("[func Fibonacci() func() int] -> %d != 8", sequenceNum)
+		if i == 6 && num != 8 {
+			t.Errorf("[func Fibonacci() func() int] -> %d != 8", num)
 		}
-		if i == 7 && sequenceNum != 13 {
-			t.Errorf("[func Fibonacci() func() int] -> %d != 13", sequenceNum)
+		if i == 7 && num != 13 {
+			t.Errorf("[func Fibonacci() func() int] -> %d != 13", num)
 		}
-		if i == 8 && sequenceNum != 21 {
-			t.Errorf("[func Fibonacci() func() int] -> %d != 21", sequenceNum)
+		if i == 8 && num != 21 {
+			t.Errorf("[func Fibonacci() func() int] -> %d != 21", num)
 		}
-		if i == 9 && sequenceNum != 34 {
-			t.Errorf("[func Fibonacci() func() int] -> %d != 34", sequenceNum)
+		if i == 9 && num != 34 {
+			t.Errorf("[func Fibonacci() func() int] -> %d != 34", num)
 		}
 	}
 }
