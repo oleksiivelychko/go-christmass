4089f5d811ac2f7caa3766f5a3ea1b15948443c4
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-11-05 18:32:28 +0200

    use var block

--- reversearray/reversearray_test.go
+++ reversearray/reversearray_test.go
@@ -6,21 +6,20 @@ import (
 	"testing"
 )
 
-func TestReverseArray(t *testing.T) {
-	var ds = make([]int, 5)
-	ds[0] = 1
-	ds[1] = 2
-	ds[2] = 3
-	ds[3] = 4
-	ds[4] = 5
+var ds = []int{1, 2, 3, 4, 5}
 
-	if !reflect.DeepEqual(reverse(ds), []int{5, 4, 3, 2, 1}) {
+func TestReverseArray(t *testing.T) {
+	var (
+		e = []int{5, 4, 3, 2, 1}
+		r = reverse(ds)
+	)
+
+	if !reflect.DeepEqual(e, r) {
 		t.Error("unable to reverse")
 	}
 }
 
 func BenchmarkReverseArray(b *testing.B) {
-	var ds = []int{1, 2, 3, 4, 5}
 	for i := 0; i < b.N; i++ {
 		sort.Ints(reverse(ds))
 	}
