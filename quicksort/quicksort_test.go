c01f06b16705bd7321d269c3b77b50a74e5a2b25
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-11-05 18:32:14 +0200

    clarify error assertion, use var block

--- quicksort/quicksort_test.go
+++ quicksort/quicksort_test.go
@@ -1,4 +1,4 @@
-package sort
+package quicksort
 
 import (
 	"reflect"
@@ -6,12 +6,19 @@ import (
 	"testing"
 )
 
-var intDS = [10]int{11, -13, 15, 17, 19, 1, 3, 5, -7, 9}
-var stringDS = [10]string{"f", "h", "a", "abc", "x", "o", "1", "u", "12", "s"}
+var (
+	intDS    = [10]int{11, -13, 15, 17, 19, 1, 3, 5, -7, 9}
+	stringDS = [10]string{"f", "h", "a", "abc", "x", "o", "1", "u", "12", "s"}
+)
 
 func TestQuickSort(t *testing.T) {
-	if !reflect.DeepEqual([]int{-13, -7, 1, 3, 5, 9, 11, 15, 17, 19}, sort(intDS[:])) {
-		t.Error("unable to sort")
+	var (
+		e      = []int{-13, -7, 1, 3, 5, 9, 11, 15, 17, 19}
+		sorted = sort(intDS[:])
+	)
+
+	if !reflect.DeepEqual(e, sorted) {
+		t.Errorf("expected %q, got %q", e, sorted)
 	}
 }
 
