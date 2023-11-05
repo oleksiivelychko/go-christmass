3bd49320e3b1cbd9b396d1047954578ee80b2803
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-11-05 18:31:28 +0200

    clarify error assertion, use var block

--- fibonacci/fibonacci_test.go
+++ fibonacci/fibonacci_test.go
@@ -3,42 +3,43 @@ package fibonacci
 import "testing"
 
 func TestFibonacci(t *testing.T) {
-	f := fibonacci()
-
-	tErr := func(num, expect int) { t.Errorf("[func fibonacci() func() int] -> %d != %d", num, expect) }
+	var (
+		tErr = func(e, g int) { t.Errorf("expected %d, got %d", e, g) }
+		f    = fibonacci()
+	)
 
 	for i := 0; i < 10; i++ {
 		num := f()
 
 		if i == 0 && num != 0 {
-			tErr(num, 0)
+			tErr(0, num)
 		}
 		if i == 1 && num != 1 {
-			tErr(num, 1)
+			tErr(1, num)
 		}
 		if i == 2 && num != 1 {
-			tErr(num, 1)
+			tErr(1, num)
 		}
 		if i == 3 && num != 2 {
-			tErr(num, 2)
+			tErr(2, num)
 		}
 		if i == 4 && num != 3 {
-			tErr(num, 3)
+			tErr(3, num)
 		}
 		if i == 5 && num != 5 {
-			tErr(num, 5)
+			tErr(5, num)
 		}
 		if i == 6 && num != 8 {
-			tErr(num, 8)
+			tErr(8, num)
 		}
 		if i == 7 && num != 13 {
-			tErr(num, 13)
+			tErr(13, num)
 		}
 		if i == 8 && num != 21 {
-			tErr(num, 21)
+			tErr(21, num)
 		}
 		if i == 9 && num != 34 {
-			tErr(num, 34)
+			tErr(34, num)
 		}
 	}
 }
