c26233d48d2cb75f40a07c63595a71eb75aa17d2
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-11-05 18:31:11 +0200

    clarify error assertion, use var block

--- dijkstrasearch/dijkstrasearch_test.go
+++ dijkstrasearch/dijkstrasearch_test.go
@@ -5,38 +5,39 @@ import (
 	"testing"
 )
 
-/*
-V(start)	-> V(a) 		= E(6)
-V(start) 	-> V(b) 		= E(2)
-V(a) 		-> V(finish)	= E(1)
-V(b) 		-> V(a) 		= E(3)
-V(b) 		-> V(finish) 	= E(5)
-*/
-var graph = map[string]map[string]float64{
-	"start": {
-		"a": 6,
-		"b": 2,
-	},
-	"a": {
-		"finish": 1,
-	},
-	"b": {
-		"a":      3,
-		"finish": 5,
-	},
-	"finish": {},
-}
-
-var costs = map[string]float64{
-	"a":      6,
-	"b":      2,
-	"finish": math.Inf(1),
-}
+var (
+	/*
+		V(start)	-> V(a) 		= E(6)
+		V(start) 	-> V(b) 		= E(2)
+		V(a) 		-> V(finish)	= E(1)
+		V(b) 		-> V(a) 		= E(3)
+		V(b) 		-> V(finish) 	= E(5)
+	*/
+	graph = map[string]map[string]float64{
+		"start": {
+			"a": 6,
+			"b": 2,
+		},
+		"a": {
+			"finish": 1,
+		},
+		"b": {
+			"a":      3,
+			"finish": 5,
+		},
+		"finish": {},
+	}
+	costs = map[string]float64{
+		"a":      6,
+		"b":      2,
+		"finish": math.Inf(1),
+	}
+)
 
 func TestDijkstraSearch(t *testing.T) {
-	c := search(graph, costs)
-	if c != 6 {
-		t.Errorf("incorrect cost %f", c)
+	cost := search(graph, costs)
+	if cost != 6.0 {
+		t.Errorf("expected %f, got %f", 6.0, cost)
 	}
 }
 
